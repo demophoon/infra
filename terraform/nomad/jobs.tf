1ad11ef3b110f8d8a884a8e522ba4485765c9bd8
Britt Gresham <britt@brittg.com>
2023-03-10 17:43:43 -0800

    Add certbot to nomad

--- terraform/nomad/jobs.tf
+++ terraform/nomad/jobs.tf
@@ -6,7 +6,10 @@ resource "nomad_job" "traefik" {
   jobspec = templatefile("${path.module}/jobspecs/traefik/traefik.hcl", {
     traefik_yaml = data.local_file.traefik_yaml.content
   })
-  hcl2 {
-    enabled = true
-  }
+  hcl2 { enabled = true }
+}
+
+resource "nomad_job" "certbot" {
+  jobspec = file("${path.module}/jobspecs/certbot/certbot.hcl")
+  hcl2 { enabled = true }
 }
